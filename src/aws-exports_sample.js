2bf126ab6e9e0408cbc62c683e0fa8a1e882a91f
Ken Preiser <WhaleGoddess@Kens-MacBook-Pro.local>
2021-02-27 13:58:14 -0800

    Add aws exports for Cakebops website

--- src/aws-exports_sample.js
+++ src/aws-exports_sample.js
@@ -1,7 +1,7 @@
 const config = {
-    'aws_project_region': '',
-    'aws_cognito_identity_pool_id': '',
-    'aws_firehose_name': ''
+    'aws_project_region': 'us-west-1',
+    'aws_cognito_identity_pool_id': 'us-west-1:ffa6916d-f83a-4bd1-b3ea-331af8dc1327',
+    'aws_firehose_name': 'arn:aws:firehose:us-west-1:406103379357:deliverystream/cakebops-app-firehose'
 };
 
 export default config;
